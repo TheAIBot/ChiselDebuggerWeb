1a25ff50c68f7f3f1da597682ddfe1fb59d72dde
TheAIBot <andreasuranus@hotmail.com>
2024-12-18 16:28:46 +0100

    Fix git changes file endings which screws up file hashes

--- scripts/WorkPacer.js
+++
@@ -1,6 +0,0 @@
-﻿var WorkPacing = {};
-WorkPacing.WaitForAnimationFrame = function () {
-    window.requestAnimationFrame(async function () {
-        await DotNet.invokeMethodAsync("ChiselDebuggerWebAsmUI", "FrameHasBeenAnimatedAsync");
-    });
-};
